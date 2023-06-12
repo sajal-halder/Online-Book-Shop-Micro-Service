f2115472668a1aae65b589008368a5efbf732151
Sajal Halder <sajal.halder@bjitacademy.com>
2023-06-12 08:01:20 +0600

    changed INVALID_TOKEN message on api gateway

--- api-gateway/src/main/java/bjit/ursa/apigateway/util/ErrorMessages.java
+++ api-gateway/src/main/java/bjit/ursa/apigateway/util/ErrorMessages.java
@@ -2,7 +2,7 @@ package bjit.ursa.apigateway.util;
 
 public  class ErrorMessages {
    public static String NO_TOKEN_FOUND = "{\"error_message\": \"No token found\"}";
-    public static String INVALID_TOKEN = "{\"error_message\": \"Invalid or Expired Token or \"}";
+    public static String INVALID_TOKEN = "{\"error_message\": \"Invalid or Expired Token \"}";
     public static String UNAUTHORIZED_MESSAGE = "{\"error_message\": \"Access denied  \"}";
 
 }
